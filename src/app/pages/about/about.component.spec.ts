c7ba48c3308c0e85c0eb2152c17dcbf3b24aa077
RESHMA PM <pm2004reshma@gmail.com>
2024-12-03 15:25:58 +0530

    Update about.component.spec.ts

--- src/app/pages/about/about.component.spec.ts
+++ src/app/pages/about/about.component.spec.ts
@@ -1,3 +1,27 @@
+aavimport { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AboutComponent } from './about.component';
+
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AboutComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
 avimport { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
