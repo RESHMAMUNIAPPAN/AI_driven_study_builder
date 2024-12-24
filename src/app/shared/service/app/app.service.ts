2d90a6c3a5fb1cb5c88c28365edac13f3ef4d498
RESHMA PM <pm2004reshma@gmail.com>
2024-12-24 20:17:56 +0530

    Update app.service.ts

--- src/app/shared/service/app/app.service.ts
+++ src/app/shared/service/app/app.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 import { TokenService } from '../token/token.service';
+import { LoginResponse } from '../../model/login.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,28 +9,49 @@ import { TokenService } from '../token/token.service';
 export class AppService {
 
   private api_url = 'http://localhost:5000';
+  private tokenService = inject(TokenService);
 
-  constructor(private http: HttpClient, private tokenService: TokenService) { }
+  constructor(private http: HttpClient, ) { }
 
-  register(body: UserData){
-    const url = this.api_url+'/api/auth/register';
-    return this.http.post(url,body);
+  register(body: UserData) {
+    const url = this.api_url + '/api/auth/register';
+    return this.http.post(url, body);
   }
 
-  login(body: LoginData){
-    const url = this.api_url+'/api/auth/login';
+  login(body: LoginData) {
+    const url = this.api_url + '/api/auth/login';
     // const headers = new HttpHeaders({
     //   Authorization: `Bearer ${this.tokenService.getToken()}`,
     // });
-    return this.http.post/*<LoginResponse>*/(url,body); // this.http.post<LoginResponse>(url,body,{headers});
+    return this.http.post<LoginResponse>(url, body); // this.http.post<LoginResponse>(url,body,{headers});
   }
 
-  /*constructor(private http:HttpClient) { }
+  /*constructor(private http:HttpClient) { }*/
 
-  generateplan(data:any){
-    const url="http://localhost:5000/ai/chat"
-    return this.http.post(url,data);
-  }*/
+  
+  generatePlan(data: any) {
+    const url = "http://localhost:5000/ai/chat";
+    console.log(this.tokenService.getToken())
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.tokenService.getToken()}`,
+    });
+    return this.http.post(url, data, {headers});
+  }
+  validateQuiz(data: any) {
+    const url = "http://localhost:5000"+"add the quiz API path here"; 
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.tokenService.getToken()}`,
+    });
+    return this.http.post(url, data, {headers}); 
+  }
+
+  generateQuiz(body:any){
+    const url = this.api_url+'/study/quiz';
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${this.tokenService.getToken()}`,
+    });
+    return this.http.post(url,body, {headers});
+  }
 }
 
 
@@ -39,6 +61,6 @@ export interface UserData {
   name: string;
 }
 export interface LoginData {
-  email:string;
-  password:string
+  email: string;
+  password: string
 }
